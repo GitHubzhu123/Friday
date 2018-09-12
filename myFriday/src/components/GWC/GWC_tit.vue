0254719652d80ff0bc954b9bfee390300f0dd2bb
lanouhn <lanouhn@feixingdediannao.local>
2018-09-12 19:41:02 +0800

    no message

---
+++ myFriday/src/components/GWC/GWC_tit.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="gwc_tit" class="zhong">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "GWC_tit"
+    }
+</script>
+
+<style scoped>
+#gwc_tit{
+  height: 95px;
+  background: greenyellow;
+}
+</style>
--- myFriday/src/router/index.js
+++ myFriday/src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+//购物车
+import GouWuChe_wu from '@/components/GouWuChe_wu'
+import GWC_tit from '@/components/GWC/GWC_tit'
+
 
 Vue.use(Router)
 
@@ -10,6 +14,17 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    }
+    },
+    //购物车
+    {
+      path: '/gouwuche_wu',
+      name: 'GouWuChe_wu',
+      component: GouWuChe_wu
+    },
+    {
+      path: '/gwc_tit',
+      name: 'GWC_tit',
+      component: GWC_tit
+    },
   ]
 })
