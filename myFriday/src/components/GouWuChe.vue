0460ae4bf04ad32a0c9ea912fc29af5b03b14e74
lanouhn <lanouhn@feixingdediannao.local>
2018-09-13 15:21:18 +0800

    no message

--- myFriday/src/components/GouWuChe.vue
+++ myFriday/src/components/GouWuChe.vue
@@ -1,20 +1,25 @@
 <template>
   <div id="gwc_wu" >
+    <top_xiao></top_xiao>
     <gwc_tit></gwc_tit>
     <hr>
     <div class="gwc_cont zhong">
       <router-view></router-view>
     </div>
-    <hr>
+    <btm></btm>
   </div>
 </template>
 
 <script>
   import GWC_tit from '@/components/GWC/GWC_tit'
+  import Top_Xiao from '@/components/Top_Xiao'
+  import Btm from '@/components/Btm'
     export default {
         name: "GouWuChe",
       components:{
         gwc_tit:GWC_tit,
+        top_xiao:Top_Xiao,
+        btm:Btm,
       }
     }
 </script>
